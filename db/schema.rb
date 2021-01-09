d63e443ba1dafdceeb0be66b15da91cd4672412a
chishakiemiko <69887263+chishakiemiko@users.noreply.github.com>
2021-01-09 18:11:49 +0900

    新規登録条件つける前

--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 2020_12_29_020002) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", default: "", null: false
-    t.string "famiry_name", default: "", null: false
+    t.string "family_name", default: "", null: false
     t.string "first_name_kana", default: "", null: false
-    t.string "famiry_name_kana", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
     t.date "birth_date"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
