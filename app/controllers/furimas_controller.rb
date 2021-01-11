8d3a7c4558fa49be1f3947bee8ce9e95e5b093b2
chishakiemiko <69887263+chishakiemiko@users.noreply.github.com>
2021-01-12 04:55:23 +0900

    新規登録条件つけた

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,10 +2,11 @@ class FurimasController < ApplicationController
   def index
     @furimas = Furima.all
   end
-end
-
+  
   def new
      @furimas = Furima.new
   end
   
+  def create
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,:birth_date, presence: true
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,:birth_date, presence: true #空では登録できない
+  
+  
 
-         
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/}
+    validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+    # 半角英字数字のみ許可する
+    validates :password, format: {with: /\A[a-z0-9]+\z/i}
+    #文字数についてパスワード６文字以上
+    validates :password, length: { minimum: 6 }
+    # VALID_PASSWORD_REGEX =/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+                          # /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}$/i
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+    validates_format_of :password, with: PASSWORD_REGEX
+    #emailは一意性
+    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\Z/i
+    validates :email, uniqueness: true
+
+  end       
 end
